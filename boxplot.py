24cf3faca97f4b678bdb908814050907b9d734cb
Celyne <dianakydd2@gmail.com>
2024-03-25 10:06:42 +0300

    updated Boxplot method

--- boxplot.py
+++ boxplot.py
@@ -9,6 +9,13 @@ df = pd.DataFrame({
 
 # print(df)
 
+'''
 df.plot(x='x',y='y',kind="box")
 plt.title("This is the box plot")
+plt.show()
+'''
+# df.plot(kind="box")
+plt.figure(figsize=(10,8))
+df.boxplot(column="y")
+plt.title("This is a Box Plot")
 plt.show()
