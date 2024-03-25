69ff9ad41fe91f5513d13bcc70328bb20b0bb53c
Celyne <dianakydd2@gmail.com>
2024-03-25 09:45:40 +0300

    Lineplot example

---
+++ lineplot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({
+
+    'x':range(1,11),
+    'y':[10,9,3,1,7,6,5,3,4,2]
+})
+
+# print(df)
+
+df.plot(x='x',y='y',kind="line")
+plt.title("This is the line plot")
+plt.show()
